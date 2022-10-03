edd0bfb1cca3c62d00b3ed5993026084be60d805
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2022-10-03 14:57:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import '../styles/App.css';
+import {useState, useEffect} from 'react';
 
 const Loader = () => <div id="loader">Loading...</div>
 
-
 const App = () => {
-
+  const [data,setData] = useState("");
+  const [loading,setLoading] = useState(true)
+  const makeURL = async (type) =>{
+    const data = await fetch(`https://www.boredapi.com/api/activity?type=${type}`)
+    const response = await data.json();
+    setLoading(false);
+    setData(response.activity);
+  }
   
+  useEffect(()=>{
+    makeURL("education")
+  },[])
 
+  const handleR =()=>{
+    makeURL("recreational")
+  }
+  const handleE =()=>{
+    makeURL("education")
+  }
   return (
-    <div id="main">
-    
+    <div id="activity">
+      {loading ? Loader() : data}
+      <br />
+      <button id="btn-recreation" onClick={handleR}>Recreational</button>
+      <button id="btn-education" onClick={handleE}>Education</button>
     </div>
   )
 }
